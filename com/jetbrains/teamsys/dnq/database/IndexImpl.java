6d19bf1f6086c3508caa2a85dc942ba1d0e84278
Vadim Gurov <vadim.gurov@gmail.com>
2009-11-25 12:49:55 +0000

    getEntityTypesToIndex
    git-svn-id: http://svn.labs.intellij.net/webr-dnq/trunk@6545 d982bc04-d64a-0410-bd9d-c7bcd499104c

--- com/jetbrains/teamsys/dnq/database/IndexImpl.java
+++ com/jetbrains/teamsys/dnq/database/IndexImpl.java
@@ -2,9 +2,15 @@ package com.jetbrains.teamsys.dnq.database;
 
 import com.jetbrains.teamsys.database.Index;
 import com.jetbrains.teamsys.database.IndexField;
+import com.jetbrains.teamsys.database.EntityIterable;
+import com.jetbrains.teamsys.database.ModelMetaData;
+import com.jetbrains.teamsys.core.dataStructures.hash.HashSet;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+
+import jetbrains.teamsys.dnq.runtime.util.DnqUtils;
 
 /**
  * Date: Nov 19, 2009
@@ -12,6 +18,7 @@ import java.util.ArrayList;
 public class IndexImpl implements Index {
 
   private List<IndexField> fields;
+  private ModelMetaData modelMetaData; 
 
   public IndexImpl() {
   }
@@ -22,6 +29,17 @@ public class IndexImpl implements Index {
 
   public List<IndexField> getFields() {
     return fields;
+  }
+
+  public Set<String> getEntityTypesToIndex() {
+    // me and inheritors
+    Set<String> res = new HashSet<String>();
+    for (IndexField f: fields) {
+      String enityType = f.getOwnerEnityType();
+      res.add(enityType);
+      DnqUtils.getModelMetaData().getEntityMetaData(enityType).getAllSubTypes(DnqUtils.getModelMetaData());
+    }
+    return res;
   }
 
   @Override
