2bb91d0b712c2f1c9238b490ae600411f7ec06d8
Maxim Mazin <Maxim.Mazin@jetbrains.com>
2009-12-01 19:13:32 +0000

    Compare operation for order associations
    git-svn-id: http://svn.labs.intellij.net/webr-dnq/trunk@6616 d982bc04-d64a-0410-bd9d-c7bcd499104c

---
+++ com/jetbrains/teamsys/dnq/association/OrderedAssociationSemantics.java
@@ -0,0 +1,54 @@
+package com.jetbrains.teamsys.dnq.association;
+
+import com.jetbrains.teamsys.database.Entity;
+import com.jetbrains.teamsys.database.TransientEntity;
+import com.jetbrains.teamsys.dnq.database.TransientStoreUtil;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * User: Maxim Mazin
+ */
+public class OrderedAssociationSemantics {
+  public static boolean compare(@Nullable Entity left, @Nullable Entity right, String orderPropertyName, int cmp) {
+    left = TransientStoreUtil.reattach((TransientEntity) left);
+    right = TransientStoreUtil.reattach((TransientEntity) right);
+
+    int cmpResult;
+    if (left == null || right == null) {
+      if (left != null) {
+        cmpResult = 1;
+      } else if (right != null) {
+        cmpResult = -1;
+      } else {
+        cmpResult = 0;
+      }
+    } else {
+      Comparable leftOrder = left.getProperty(orderPropertyName);
+      Comparable rightOrder = right.getProperty(orderPropertyName);
+      cmpResult = leftOrder.compareTo(rightOrder);
+    }
+
+    switch (cmp) {
+      case 1: return cmpResult >= 0;
+      case 2: return cmpResult < 0;
+      case 3: return cmpResult <= 0;
+      default: return cmpResult > 0;
+    }
+  }
+
+
+  public static void swap(@Nullable Entity left, @Nullable Entity right, String orderPropertyName) {
+    left = TransientStoreUtil.reattach((TransientEntity) left);
+    right = TransientStoreUtil.reattach((TransientEntity) right);
+
+    if (left != null && right != null) {
+      Comparable leftOrder = left.getProperty(orderPropertyName);
+      Comparable rightOrder = right.getProperty(orderPropertyName);
+
+      if (leftOrder != null && rightOrder != null) {
+        left.setProperty(orderPropertyName, rightOrder);
+        right.setProperty(orderPropertyName, leftOrder);     
+      }
+    }
+  }
+}
