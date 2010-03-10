88ad57ef0bd7ba5065bcd6009ae5e9490763a2e3
Vyacheslav Lukianov <lvo@jetbrains.com>
2010-03-10 16:07:18 +0000

    DNQ: reduction of the take() sequence operation
    git-svn-id: http://svn.labs.intellij.net/webr-dnq/trunk@7504 d982bc04-d64a-0410-bd9d-c7bcd499104c

--- com/jetbrains/teamsys/dnq/database/PersistentEntityIterableWrapper.java
+++ com/jetbrains/teamsys/dnq/database/PersistentEntityIterableWrapper.java
@@ -1,10 +1,8 @@
 package com.jetbrains.teamsys.dnq.database;
 
 import com.jetbrains.teamsys.database.*;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.jetbrains.annotations.NotNull;
 import jetbrains.springframework.configuration.runtime.ServiceLocator;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Wrapper for persistent iterable. Handles iterator.next and delegates it to transient session.
@@ -13,83 +11,87 @@ import jetbrains.springframework.configuration.runtime.ServiceLocator;
  */
 class PersistentEntityIterableWrapper implements EntityIterable {
 
-  private EntityIterable wrappedIterable;
+    private EntityIterable wrappedIterable;
 
-  PersistentEntityIterableWrapper(@NotNull EntityIterable wrappedIterable) {
-    if (wrappedIterable instanceof PersistentEntityIterableWrapper) {
-      throw new IllegalArgumentException("Can't wrap transient entity iterable with another transient entity iterable.");
+    PersistentEntityIterableWrapper(@NotNull EntityIterable wrappedIterable) {
+        if (wrappedIterable instanceof PersistentEntityIterableWrapper) {
+            throw new IllegalArgumentException("Can't wrap transient entity iterable with another transient entity iterable.");
+        }
+
+        this.wrappedIterable = wrappedIterable;
     }
 
-    this.wrappedIterable = wrappedIterable;
-  }
+    public long size() {
+        return wrappedIterable.size();
+    }
 
-  public long size() {
-    return wrappedIterable.size();
-  }
+    public long count() {
+        return wrappedIterable.count();
+    }
 
-  public long count() {
-    return wrappedIterable.count();
-  }
+    public int indexOf(@NotNull Entity entity) {
+        return wrappedIterable.indexOf(entity);
+    }
 
-  public int indexOf(@NotNull Entity entity) {
-    return wrappedIterable.indexOf(entity);
-  }
+    public boolean contains(@NotNull Entity entity) {
+        return wrappedIterable.contains(entity);
+    }
 
-  public boolean contains(@NotNull Entity entity) {
-    return wrappedIterable.contains(entity);
-  }
+    @NotNull
+    public EntityIterableHandle getHandle() {
+        return wrappedIterable.getHandle();
+    }
 
-  @NotNull
-  public EntityIterableHandle getHandle() {
-    return wrappedIterable.getHandle();
-  }
+    @NotNull
+    public EntityIterable intersect(@NotNull EntityIterable right) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.intersect(right.getSource()));
+    }
 
-  @NotNull
-  public EntityIterable intersect(@NotNull EntityIterable right) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.intersect(right.getSource()));
-  }
+    @NotNull
+    public EntityIterable intersectSavingOrder(@NotNull EntityIterable right) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.intersectSavingOrder(right.getSource()));
+    }
 
-  @NotNull
-  public EntityIterable intersectSavingOrder(@NotNull EntityIterable right) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.intersectSavingOrder(right.getSource()));
-  }
+    @NotNull
+    public EntityIterable union(@NotNull EntityIterable right) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.union(right.getSource()));
+    }
 
-  @NotNull
-  public EntityIterable union(@NotNull EntityIterable right) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.union(right.getSource()));
-  }
+    @NotNull
+    public EntityIterable minus(@NotNull EntityIterable right) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.minus(right.getSource()));
+    }
 
-  @NotNull
-  public EntityIterable minus(@NotNull EntityIterable right) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.minus(right.getSource()));
-  }
+    @NotNull
+    public EntityIterable concat(@NotNull EntityIterable right) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.concat(right.getSource()));
+    }
 
-  @NotNull
-  public EntityIterable concat(@NotNull EntityIterable right) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.concat(right.getSource()));
-  }
+    public EntityIterable skip(int number) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.skip(number));
+    }
 
-  public EntityIterable skip(int number) {
-    return new PersistentEntityIterableWrapper(wrappedIterable.skip(number));
-  }
+    public EntityIterable take(int number) {
+        return new PersistentEntityIterableWrapper(wrappedIterable.take(number));
+    }
 
-  public boolean isSortResult() {
-    return wrappedIterable.isSortResult();
-  }
+    public boolean isSortResult() {
+        return wrappedIterable.isSortResult();
+    }
 
-  public EntityIterable asSortResult() {
-    return new PersistentEntityIterableWrapper(wrappedIterable.asSortResult());
-  }
+    public EntityIterable asSortResult() {
+        return new PersistentEntityIterableWrapper(wrappedIterable.asSortResult());
+    }
 
-  @NotNull
-  public EntityIterable getSource() {
-    return wrappedIterable;
-  }
+    @NotNull
+    public EntityIterable getSource() {
+        return wrappedIterable;
+    }
 
-  public EntityIterator iterator() {
-    return new PersistentEntityIteratorWrapper(wrappedIterable.iterator(),
-           (TransientStoreSession) ((TransientEntityStore) ServiceLocator.getBean("transientEntityStore")).getThreadSession());
-  }
+    public EntityIterator iterator() {
+        return new PersistentEntityIteratorWrapper(wrappedIterable.iterator(),
+                (TransientStoreSession) ((TransientEntityStore) ServiceLocator.getBean("transientEntityStore")).getThreadSession());
+    }
 
     public boolean isEmpty() {
         return wrappedIterable.isEmpty();
--- com/jetbrains/teamsys/dnq/database/TransientEntityIterable.java
+++ com/jetbrains/teamsys/dnq/database/TransientEntityIterable.java
@@ -17,95 +17,99 @@ import java.util.Set;
  */
 public class TransientEntityIterable implements EntityIterable {
 
-  private static final Log log = LogFactory.getLog(TransientEntityIterable.class);
+    private static final Log log = LogFactory.getLog(TransientEntityIterable.class);
 
-  private Set<TransientEntity> values;
-  //@NotNull private final EntityIterable source;
+    private Set<TransientEntity> values;
+    //@NotNull private final EntityIterable source;
 
-  public TransientEntityIterable(@NotNull Set<TransientEntity> values
-                                 /*@NotNull final EntityIterable source*/) {
-    this.values = values;
-    //this.source = source;
-  }
-
-  public long size() {
-    if (log.isWarnEnabled()) {
-      log.warn("Size is requested from TransientEntityIterable!");
+    public TransientEntityIterable(@NotNull Set<TransientEntity> values
+                                   /*@NotNull final EntityIterable source*/) {
+        this.values = values;
+        //this.source = source;
     }
-    return values.size();
-  }
 
-  public long count() {
-    if (log.isWarnEnabled()) {
-      log.warn("Count is requested from TransientEntityIterable!");
+    public long size() {
+        if (log.isWarnEnabled()) {
+            log.warn("Size is requested from TransientEntityIterable!");
+        }
+        return values.size();
     }
-    return values.size();
-  }
 
-  public int indexOf(@NotNull Entity entity) {
-    return Arrays.asList(values.toArray(new Entity[values.size()])).indexOf(entity);
-  }
-
-  public boolean contains(@NotNull Entity entity) {
-    return values.contains(entity);
-  }
-    
-  @NotNull
-  public EntityIterableHandle getHandle() {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-    //return source.getHandle();
-  }
-
-  @NotNull
-  public EntityIterable intersect(@NotNull EntityIterable right) {
-    //return source.intersect(right);
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  @NotNull
-  public EntityIterable intersectSavingOrder(@NotNull EntityIterable right) {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  @NotNull
-  public EntityIterable union(@NotNull EntityIterable right) {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  @NotNull
-  public EntityIterable minus(@NotNull EntityIterable right) {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  @NotNull
-  public EntityIterable concat(@NotNull EntityIterable right) {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  public EntityIterable skip(int number) {
-    return new TransientEntityIterable(SequenceOperations.toSet(SequenceOperations.skip(values, number)));
-  }
-
-  public boolean isSortResult() {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  public EntityIterable asSortResult() {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-  }
-
-  @NotNull
-  public EntityIterable getSource() {
-    throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
-    //return source;
-  }
-
-  public EntityIterator iterator() {
-    if (log.isTraceEnabled()) {
-      log.trace("New iterator requested for transient iterable " + this);
+    public long count() {
+        if (log.isWarnEnabled()) {
+            log.warn("Count is requested from TransientEntityIterable!");
+        }
+        return values.size();
     }
-    return new TransientEntityIterator(values.iterator());
-  }
+
+    public int indexOf(@NotNull Entity entity) {
+        return Arrays.asList(values.toArray(new Entity[values.size()])).indexOf(entity);
+    }
+
+    public boolean contains(@NotNull Entity entity) {
+        return values.contains(entity);
+    }
+
+    @NotNull
+    public EntityIterableHandle getHandle() {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+        //return source.getHandle();
+    }
+
+    @NotNull
+    public EntityIterable intersect(@NotNull EntityIterable right) {
+        //return source.intersect(right);
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    @NotNull
+    public EntityIterable intersectSavingOrder(@NotNull EntityIterable right) {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    @NotNull
+    public EntityIterable union(@NotNull EntityIterable right) {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    @NotNull
+    public EntityIterable minus(@NotNull EntityIterable right) {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    @NotNull
+    public EntityIterable concat(@NotNull EntityIterable right) {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    public EntityIterable skip(int number) {
+        return new TransientEntityIterable(SequenceOperations.toSet(SequenceOperations.skip(values, number)));
+    }
+
+    public EntityIterable take(int number) {
+        return new TransientEntityIterable(SequenceOperations.toSet(SequenceOperations.take(values, number)));
+    }
+
+    public boolean isSortResult() {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    public EntityIterable asSortResult() {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+    }
+
+    @NotNull
+    public EntityIterable getSource() {
+        throw new UnsupportedOperationException("Not supported by TransientEntityIterable");
+        //return source;
+    }
+
+    public EntityIterator iterator() {
+        if (log.isTraceEnabled()) {
+            log.trace("New iterator requested for transient iterable " + this);
+        }
+        return new TransientEntityIterator(values.iterator());
+    }
 
     public boolean isEmpty() {
         return size() == 0;
