692c145ecc7ae9e0c205811269f464d85920c892
Vadim Gurov <vadim.gurov@gmail.com>
2011-08-02 11:31:31 +0000

    rollback property and string blob changes on set to original value
    git-svn-id: http://svn.labs.intellij.net/webr-dnq/trunk@13326 d982bc04-d64a-0410-bd9d-c7bcd499104c

---
+++ com/jetbrains/teamsys/dnq/database/SimplePropertyTest.java
@@ -0,0 +1,147 @@
+package com.jetbrains.teamsys.dnq.database;
+
+import com.jetbrains.mps.dnq.common.tests.AbstractEntityStoreAwareTestCase;
+import com.jetbrains.mps.dnq.common.tests.TestOnlyServiceLocator;
+import com.jetbrains.teamsys.database.TransientEntity;
+import com.jetbrains.teamsys.database.TransientEntityStore;
+import com.jetbrains.teamsys.database.TransientStoreSession;
+import com.jetbrains.teamsys.dnq.association.PrimitiveAssociationSemantics;
+import junit.framework.Assert;
+
+public class SimplePropertyTest extends AbstractEntityStoreAwareTestCase {
+
+  // string
+
+  public void testSetString_Null_Empty_Null() {
+    checkStringValue1Value2Value1MakesNoChanges(null, "");
+  }
+
+  public void testSetString_Empty_NotEmpty_Empty() {
+    checkStringValue1Value2Value1MakesNoChanges("", "test");
+  }
+
+  public void testSetString_Empty_Empty_Empty() {
+    checkStringValue1Value2Value1MakesNoChanges("", "");
+  }
+
+  public void testSetString_Empty_Null_Empty() {
+    checkStringValue1Value2Value1MakesNoChanges("", null);
+  }
+
+  public void testSetString_Null_NotEmpty_Null() {
+    checkStringValue1Value2Value1MakesNoChanges(null, "test1");
+  }
+
+  public void testSetString_Null_Null_Null() {
+    checkStringValue1Value2Value1MakesNoChanges(null, null);
+  }
+
+  public void testSetString_NotEmpty_Null_NotEmpty() {
+    checkStringValue1Value2Value1MakesNoChanges("test", null);
+  }
+
+  public void testSetString_NotEmpty_Empty_NotEmpty() {
+    checkStringValue1Value2Value1MakesNoChanges("test", "");
+  }
+
+  public void testSetString_NotEmpty_NotEmpty_NotEmpty() {
+    checkStringValue1Value2Value1MakesNoChanges("test", "test1");
+  }
+
+  // text
+  
+  public void testSetText_Null_Empty_Null() {
+    checkTextValue1Value2Value1MakesNoChanges(null, "");
+  }
+
+  public void testSetText_Empty_NotEmpty_Empty() {
+    checkTextValue1Value2Value1MakesNoChanges("", "test");
+  }
+
+  public void testSetText_Empty_Empty_Empty() {
+    checkTextValue1Value2Value1MakesNoChanges("", "");
+  }
+
+  public void testSetText_Empty_Null_Empty() {
+    checkTextValue1Value2Value1MakesNoChanges("", null);
+  }
+
+  public void testSetText_Null_NotEmpty_Null() {
+    checkTextValue1Value2Value1MakesNoChanges(null, "test1");
+  }
+
+  public void testSetText_Null_Null_Null() {
+    checkTextValue1Value2Value1MakesNoChanges(null, null);
+  }
+
+  public void testSetText_NotEmpty_Null_NotEmpty() {
+    checkTextValue1Value2Value1MakesNoChanges("test", null);
+  }
+
+  public void testSetText_NotEmpty_Empty_NotEmpty() {
+    checkTextValue1Value2Value1MakesNoChanges("test", "");
+  }
+
+  public void testSetText_NotEmpty_NotEmpty_NotEmpty() {
+    checkTextValue1Value2Value1MakesNoChanges("test", "test1");
+  }
+
+  private void checkStringValue1Value2Value1MakesNoChanges(String value1, String value2) {
+    TransientEntityStore store = TestOnlyServiceLocator.getTransientEntityStore();
+    TransientStoreSession transientSession = store.beginSession("t1", new Object());
+    TransientEntity e = null;
+    try {
+      e = (TransientEntity) TestOnlyServiceLocator.getTransientEntityStore().getThreadSession().newEntity("Issue");
+      PrimitiveAssociationSemantics.set(e, "summary", value1, String.class);
+      transientSession.intermediateCommit();
+
+      Assert.assertEquals(value1, PrimitiveAssociationSemantics.get(e, "summary", String.class, null));
+
+      PrimitiveAssociationSemantics.set(e, "summary", value2, String.class);
+      PrimitiveAssociationSemantics.set(e, "summary", value1, String.class);
+      PrimitiveAssociationSemantics.set(e, "summary", value2, String.class);
+      PrimitiveAssociationSemantics.set(e, "summary", value1, String.class);
+
+      Assert.assertEquals(0, transientSession.getTransientChangesTracker().getChanges().size());
+      Assert.assertEquals(false, EntityOperations.hasChanges((TransientEntity) e));
+      Assert.assertEquals(false, EntityOperations.hasChanges((TransientEntity) e, "summary"));
+
+      transientSession.intermediateCommit();
+      Assert.assertEquals(value1, PrimitiveAssociationSemantics.get(e, "summary", String.class, null));
+    } catch (Throwable ex) {
+      TransientStoreUtil.abort(ex, transientSession);
+    } finally {
+      TransientStoreUtil.commit(transientSession);
+    }
+  }
+
+  private void checkTextValue1Value2Value1MakesNoChanges(String value1, String value2) {
+    TransientEntityStore store = TestOnlyServiceLocator.getTransientEntityStore();
+    TransientStoreSession transientSession = store.beginSession("t1", new Object());
+    TransientEntity e = null;
+    try {
+      e = (TransientEntity) TestOnlyServiceLocator.getTransientEntityStore().getThreadSession().newEntity("Issue");
+      PrimitiveAssociationSemantics.setBlob(e, "description", value1);
+      transientSession.intermediateCommit();
+
+      Assert.assertEquals(value1, PrimitiveAssociationSemantics.getBlobAsString(e, "description"));
+
+      PrimitiveAssociationSemantics.setBlob(e, "description", value2);
+      PrimitiveAssociationSemantics.setBlob(e, "description", value1);
+      PrimitiveAssociationSemantics.setBlob(e, "description", value2);
+      PrimitiveAssociationSemantics.setBlob(e, "description", value1);
+
+      Assert.assertEquals(0, transientSession.getTransientChangesTracker().getChanges().size());
+      Assert.assertEquals(false, EntityOperations.hasChanges((TransientEntity) e));
+      Assert.assertEquals(false, EntityOperations.hasChanges((TransientEntity) e, "description"));
+
+      transientSession.intermediateCommit();
+      Assert.assertEquals(value1, PrimitiveAssociationSemantics.getBlobAsString(e, "description"));
+    } catch (Throwable ex) {
+      TransientStoreUtil.abort(ex, transientSession);
+    } finally {
+      TransientStoreUtil.commit(transientSession);
+    }
+  }
+
+}
