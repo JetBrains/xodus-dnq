a7185c59576a1ed958ff6da4311290a320a217d8
Vyacheslav Lukianov <lvo@intellij.net>
2015-12-18 15:46:44 +0300

    minor: prevent creation of the class

--- jetbrains/exodus/entitystore/TxnUtil.java
+++ jetbrains/exodus/entitystore/TxnUtil.java
@@ -2,6 +2,9 @@ package jetbrains.exodus.entitystore;
 
 public class TxnUtil {
 
+    private TxnUtil() {
+    }
+
     public static void registerTransation(PersistentEntityStoreImpl store, PersistentStoreTransaction txn) {
         store.registerTransaction(txn);
     }
