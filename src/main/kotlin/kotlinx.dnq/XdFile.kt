4caeb0eabb3fac02f3f2de3b477ebc892f83d22e
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-01-16 11:42:44 +0100

    Added resize and others method to XdFile

--- src/main/kotlin/kotlinx.dnq/XdFile.kt
+++ src/main/kotlin/kotlinx.dnq/XdFile.kt
@@ -1,8 +1,10 @@
 package kotlinx.dnq
 
 import jetbrains.exodus.entitystore.Entity
+import jetbrains.teamsys.dnq.runtime.files.FileMeta
 import jetbrains.teamsys.dnq.runtime.files.PersistentFileImpl
 import kotlinx.dnq.simple.containsNone
+import java.io.InputStream
 
 class XdFile(override val entity: Entity) : XdEntity() {
     companion object : XdLegacyEntityType<PersistentFileImpl, XdFile>()
@@ -14,4 +16,22 @@ class XdFile(override val entity: Entity) : XdEntity() {
     var charset by xdStringProp()
     var mimeType by xdStringProp()
     var trusted by xdBooleanProp()
+
+    var baseName: String
+        get() = mpsType.getBaseName(entity)
+        set(value) = mpsType.setBaseName(value, entity)
+
+    val stringContent: String
+        get() = mpsType.getStringContent(entity)
+
+    val isImage: Boolean
+        get() = mpsType.isImage(entity)
+
+    fun getThumbnail(maxWidth: Int, maxHeight: Int, cropped: Boolean, fileMeta: FileMeta?): InputStream {
+        return mpsType.getThumbnail(maxWidth, maxHeight, cropped, fileMeta, entity)
+    }
+
+    fun resize(maxWidth: Int, maxHeight: Int, crop: Boolean = false) {
+        mpsType.resize(maxWidth, maxHeight, crop, entity)
+    }
 }
