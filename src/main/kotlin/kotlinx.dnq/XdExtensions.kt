b65b38bc9c28e66f7cc9f784628c4bccd10a1383
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-02-13 11:21:38 +0100

    Entity.wrap(XdEntityType)

--- src/main/kotlin/kotlinx.dnq/XdExtensions.kt
+++ src/main/kotlin/kotlinx.dnq/XdExtensions.kt
@@ -5,4 +5,7 @@ import jetbrains.exodus.entitystore.Entity
 val Entity.wrapper: XdEntity get() = XdModel.wrap(this)
 
 @Suppress("UNCHECKED_CAST")
+@Deprecated("Use wrap(entityType)", replaceWith = ReplaceWith("this.wrap(T)"))
 fun <T : XdEntity> Entity.wrapper(): T = this.wrapper as T
+
+fun <T : XdEntity> Entity.wrap(entityType: XdEntityType<T>): T = entityType.wrap(this)
