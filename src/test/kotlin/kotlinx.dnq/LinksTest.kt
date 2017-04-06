4f30c256082dc4861bcb42d6c9a0c85e22a2a02b
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-04-06 20:20:12 +0200

    Added tests for get{Added, Removed}Links()

--- src/test/kotlin/kotlinx.dnq/LinksTest.kt
+++ src/test/kotlin/kotlinx.dnq/LinksTest.kt
@@ -1,6 +1,13 @@
 package kotlinx.dnq
 
 import kotlinx.dnq.query.*
+import kotlinx.dnq.util.getAddedLinks
+import kotlinx.dnq.util.getOldValue
+import kotlinx.dnq.util.getRemovedLinks
+import org.hamcrest.CustomMatcher
+import org.hamcrest.collection.IsEmptyCollection
+import org.hamcrest.collection.IsIterableContainingInOrder
+import org.junit.Assert
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -63,5 +70,43 @@ class LinksTest : DBTest() {
         store.transactional {
             assertEquals("1@1.com", user.contacts.first().email)
         }
+    }
+
+    @Test
+    fun `getAddedLinks should return added links`() {
+        val user = store.transactional {
+            User.new {
+                login = "zeckson"
+                skill = 1
+            }
+        }
+        store.transactional {
+            user.contacts.add(Contact.new { email = "zeckson@spb.ru" })
+            Assert.assertThat(user.getAddedLinks(User::contacts).toList(), IsIterableContainingInOrder(listOf(contactMatcher { it.email == "zeckson@spb.ru" })))
+            Assert.assertThat(user.getRemovedLinks(User::contacts).toList(), IsEmptyCollection())
+        }
+    }
+
+    @Test
+    fun `getRemovedLinks should return removed links`() {
+        val user = store.transactional {
+            User.new {
+                login = "zeckson"
+                skill = 1
+                contacts.add(Contact.new { email = "zeckson@spb.ru" })
+            }
+        }
+        store.transactional {
+            val contact = user.contacts.first()
+            user.contacts.remove(contact)
+            contact.delete()
+
+            Assert.assertThat(user.getAddedLinks(User::contacts).toList(), IsEmptyCollection())
+            Assert.assertThat(user.getRemovedLinks(User::contacts).toList(), IsIterableContainingInOrder(listOf(contactMatcher { it.getOldValue(Contact::email) == "zeckson@spb.ru" })))
+        }
+    }
+
+    private fun contactMatcher(match: (Contact) -> Boolean) = object : CustomMatcher<DBTest.Contact>("email contact") {
+        override fun matches(item: Any?) = item is Contact && match(item)
     }
 }
