e0ac1a342890f2466cd3bfbb8687e7004498154a
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-08-17 14:17:27 +0200

    Updated infer-hierarchy-type-parameter to 1.0.1

--- src/main/kotlin/kotlinx.dnq/XdLegacyEntityType.kt
+++ src/main/kotlin/kotlinx.dnq/XdLegacyEntityType.kt
@@ -3,24 +3,12 @@ package kotlinx.dnq
 import com.jetbrains.teamsys.dnq.database.BasePersistentClassImpl
 import jetbrains.teamsys.dnq.runtime.util.DnqUtils
 import kotlinx.dnq.store.container.LegacyStoreContainer
-import org.jetbrains.mazine.infer.type.parameter.inferTypeParameter
-import java.lang.reflect.ParameterizedType
+import org.jetbrains.mazine.infer.type.parameter.inferTypeParameterClass
 
 
 abstract class XdLegacyEntityType<P : BasePersistentClassImpl, T : XdEntity>(legacyClass: Class<P>? = null) : XdEntityType<T>(LegacyStoreContainer) {
 
-    val legacyClass: Class<P> = legacyClass ?: run {
-        val pArgument = inferTypeParameter(XdLegacyEntityType::class.java, "P", javaClass)
-
-        val clazz = when (pArgument) {
-            is Class<*> -> pArgument
-            is ParameterizedType -> pArgument.rawType as? Class<*>
-            else -> null
-        }
-
-        @Suppress("UNCHECKED_CAST")
-        ((clazz as? Class<P>) ?: throw IllegalArgumentException("Cannot infer legacy MPS class for ${javaClass.canonicalName}"))
-    }
+    val legacyClass: Class<P> = legacyClass ?: inferTypeParameterClass(XdLegacyEntityType::class.java, "P", javaClass)
 
     override val entityType: String
         get() = legacyClass.entityType
