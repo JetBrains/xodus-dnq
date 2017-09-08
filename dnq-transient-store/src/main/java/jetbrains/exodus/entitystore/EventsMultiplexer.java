50910768a6aeec409956a4fe1686003e1d7a5e87
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-09-08 13:53:32 +0200

    EventsMultiplexer should not control asyncJobProcessor's life-cycle

--- dnq-transient-store/src/main/java/jetbrains/exodus/entitystore/EventsMultiplexer.java
+++ dnq-transient-store/src/main/java/jetbrains/exodus/entitystore/EventsMultiplexer.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2006 - 2017 JetBrains s.r.o.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,6 +41,7 @@ public class EventsMultiplexer implements TransientStoreSessionListener, IEvents
     private Map<String, Queue<IEntityListener>> typeToListeners = new HashMap<>();
     private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
     private boolean open = true;
+    @Nullable
     private final JobProcessor eventsMultiplexerJobProcessor;
 
     public EventsMultiplexer() {
@@ -186,16 +187,6 @@ public class EventsMultiplexer implements TransientStoreSessionListener, IEvents
     }
 
     public void onClose(@NotNull TransientEntityStore store) {
-        if (logger.isInfoEnabled()) {
-            logger.info("Finishing EventsMultiplexer job processor for store " + store);
-        }
-        JobProcessor processor = getAsyncJobProcessor();
-        if (processor != null && !(processor.isFinished())) {
-            processor.finish();
-            if (logger.isInfoEnabled()) {
-                logger.info("EventsMultiplexer closed. Jobs count: " + processor.pendingJobs() + "/" + processor.pendingTimedJobs());
-            }
-        }
     }
 
     public boolean hasEntityListeners() {
@@ -356,13 +347,9 @@ public class EventsMultiplexer implements TransientStoreSessionListener, IEvents
         }
     }
 
+    @Nullable
     public JobProcessor getAsyncJobProcessor() {
-        JobProcessor processor = this.eventsMultiplexerJobProcessor;
-        if (processor != null && !(processor.isFinished())) {
-            processor.setExceptionHandler(EX_HANDLER);
-            processor.start();
-        }
-        return processor;
+        return this.eventsMultiplexerJobProcessor;
     }
 
     private static class JobImpl extends Job {
