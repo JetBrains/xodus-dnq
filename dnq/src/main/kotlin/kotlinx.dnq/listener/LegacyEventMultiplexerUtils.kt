1b3470e948d2774644046557de445c4d1ac02588
pavel.nikitin <pavel.nikitin@jetbrains.com>
2017-09-19 13:33:34 +0200

    fix listener leak (added delegating equals and hashCode for listener wrapper)

--- dnq/src/main/kotlin/kotlinx.dnq/listener/LegacyEventMultiplexerUtils.kt
+++ dnq/src/main/kotlin/kotlinx.dnq/listener/LegacyEventMultiplexerUtils.kt
@@ -50,21 +50,25 @@ fun <XD : XdEntity> XdEntityType<XD>.removeListener(store: TransientEntityStore,
     store.eventsMultiplexer?.removeListener(this, listener)
 }
 
-fun <XD : XdEntity> XdEntityListener<XD>.asLegacyListener() = let {
-    object : IEntityListener<Entity> {
-        override fun addedSyncBeforeConstraints(added: Entity) = it.addedSyncBeforeConstraints(added.toXd<XD>())
-        override fun addedSyncBeforeFlush(added: Entity) = it.addedSyncBeforeFlush(added.toXd<XD>())
-        override fun addedSync(added: Entity) = it.addedSync(added.toXd<XD>())
-        override fun addedAsync(added: Entity) = it.addedAsync(added.toXd<XD>())
+fun <XD : XdEntity> XdEntityListener<XD>.asLegacyListener(): IEntityListener<Entity> = EntityListenerWrapper(this)
 
-        override fun updatedSyncBeforeConstraints(old: Entity, current: Entity) = it.updatedSyncBeforeConstraints(old.toXd<XD>(), current.toXd<XD>())
-        override fun updatedSyncBeforeFlush(old: Entity, current: Entity) = it.updatedSyncBeforeFlush(old.toXd<XD>(), current.toXd<XD>())
-        override fun updatedSync(old: Entity, current: Entity) = it.updatedSync(old.toXd<XD>(), current.toXd<XD>())
-        override fun updatedAsync(old: Entity, current: Entity) = it.updatedAsync(old.toXd<XD>(), current.toXd<XD>())
+internal class EntityListenerWrapper<XD : XdEntity>(val wrapped: XdEntityListener<XD>) : IEntityListener<Entity> {
+    override fun addedSyncBeforeConstraints(added: Entity) = wrapped.addedSyncBeforeConstraints(added.toXd<XD>())
+    override fun addedSyncBeforeFlush(added: Entity) = wrapped.addedSyncBeforeFlush(added.toXd<XD>())
+    override fun addedSync(added: Entity) = wrapped.addedSync(added.toXd<XD>())
+    override fun addedAsync(added: Entity) = wrapped.addedAsync(added.toXd<XD>())
 
-        override fun removedSyncBeforeConstraints(removed: Entity) = it.removedSyncBeforeConstraints(removed.toXd<XD>())
-        override fun removedSyncBeforeFlush(removed: Entity) = it.removedSyncBeforeFlush(removed.toXd<XD>())
-        override fun removedSync(removed: Entity) = it.removedSync(removed.toXd<XD>())
-        override fun removedAsync(removed: Entity) = it.removedAsync(removed.toXd<XD>())
-    }
+    override fun updatedSyncBeforeConstraints(old: Entity, current: Entity) = wrapped.updatedSyncBeforeConstraints(old.toXd<XD>(), current.toXd<XD>())
+    override fun updatedSyncBeforeFlush(old: Entity, current: Entity) = wrapped.updatedSyncBeforeFlush(old.toXd<XD>(), current.toXd<XD>())
+    override fun updatedSync(old: Entity, current: Entity) = wrapped.updatedSync(old.toXd<XD>(), current.toXd<XD>())
+    override fun updatedAsync(old: Entity, current: Entity) = wrapped.updatedAsync(old.toXd<XD>(), current.toXd<XD>())
+
+    override fun removedSyncBeforeConstraints(removed: Entity) = wrapped.removedSyncBeforeConstraints(removed.toXd<XD>())
+    override fun removedSyncBeforeFlush(removed: Entity) = wrapped.removedSyncBeforeFlush(removed.toXd<XD>())
+    override fun removedSync(removed: Entity) = wrapped.removedSync(removed.toXd<XD>())
+    override fun removedAsync(removed: Entity) = wrapped.removedAsync(removed.toXd<XD>())
+
+    override fun hashCode() = wrapped.hashCode()
+
+    override fun equals(other: Any?) = other is EntityListenerWrapper<*> && wrapped == other.wrapped
 }
