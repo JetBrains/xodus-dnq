f015753f3e0a8fc4c9926dd2db8912e62f51989e
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-10-07 18:24:07 +0200

    Implemented caching for simple properties delegates

--- dnq/src/main/kotlin/kotlinx.dnq/util/XdPropertyCachedProvider.kt
+++ dnq/src/main/kotlin/kotlinx.dnq/util/XdPropertyCachedProvider.kt
@@ -1,19 +1,16 @@
 package kotlinx.dnq.util
 
 import kotlinx.dnq.XdEntity
-import kotlinx.dnq.simple.XdConstrainedProperty
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KProperty
 
-class XdPropertyCachedProvider<in R : XdEntity, T>(private val create: () -> XdConstrainedProperty<R, T>) {
+class XdPropertyCachedProvider<out D>(private val create: () -> D) {
     companion object {
-        val cache = ConcurrentHashMap<KProperty<*>, XdConstrainedProperty<*, *>>()
-
-        fun <V : XdConstrainedProperty<*, *>> getOrPut(prop: KProperty<*>, defaultValue: () -> V): V {
-            @Suppress("UNCHECKED_CAST")
-            return cache.getOrPut(prop, defaultValue) as V
-        }
+        val cache = ConcurrentHashMap<KProperty<*>, Any>()
     }
 
-    operator fun provideDelegate(thisRef: R, prop: KProperty<*>) = getOrPut(prop, create)
+    operator fun provideDelegate(thisRef: XdEntity, prop: KProperty<*>): D {
+        @Suppress("UNCHECKED_CAST")
+        return cache.getOrPut(prop, create) as D
+    }
 }
