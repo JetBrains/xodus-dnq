60f7c191c9b1ad0b0049c2bb6ed689440ce6a1d5
Pavel Nikitin <pavel.nikitin@jetbrains.com>
2017-10-24 13:15:42 +0200

    add remaining bits of TargetDelete test

--- dnq/src/test/kotlin/kotlinx.dnq/onTargetDelete/Model.kt
+++ dnq/src/test/kotlin/kotlinx.dnq/onTargetDelete/Model.kt
@@ -1,10 +1,9 @@
 package kotlinx.dnq.onTargetDelete
 
 import jetbrains.exodus.entitystore.Entity
-import kotlinx.dnq.XdEntity
-import kotlinx.dnq.XdNaturalEntityType
+import kotlinx.dnq.*
 import kotlinx.dnq.link.OnDeletePolicy.CASCADE
-import kotlinx.dnq.xdLink1
+import kotlinx.dnq.link.OnDeletePolicy.CLEAR
 
 fun A1(a3: A3) = A1.new { l = a3 }
 
@@ -20,4 +19,86 @@ class A2(entity: Entity) : A3(entity) {
 
 open class A3(override val entity: Entity) : XdEntity() {
     companion object : XdNaturalEntityType<A3>()
+}
+
+fun B1(b2: B2) = B1.new { this.b2 = b2 }
+
+class B1(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<B1>()
+
+    var b2 by xdLink1(B2, onTargetDelete = CASCADE)
+}
+
+fun B2(b3: B3) = B2.new { this.b3.add(b3) }
+
+class B2(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<B2>()
+
+    val b3 by xdLink0_N(B3::b2, onTargetDelete = CASCADE, onDelete = CASCADE)
+}
+
+fun B3(b4: B4) = B3.new { this.b4.add(b4) }
+
+class B3(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<B3>()
+
+    val b4 by xdLink1_N(B4::b3, onTargetDelete = CASCADE)
+    val b2: B2? by xdLink0_1(B2::b3)
+}
+
+class B4(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<B4>()
+
+    var b3: B3 by xdLink1(B3::b4)
+}
+
+class C1(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<C1>()
+
+    var c2: C2? by xdLink0_1(C2::c1)
+}
+
+fun C2(c1: C1) = C2.new { this.c1 = c1 }
+
+class C2(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<C2>()
+
+    var c1 by xdLink0_1(C1::c2, onTargetDelete = CLEAR)
+}
+
+class D1(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<D1>()
+
+    val d2 by xdLink0_N(D2, onDelete = CASCADE, onTargetDelete = CLEAR)
+}
+
+fun D2(name: String) = D2.new { this.name = name }
+
+class D2(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<D2>()
+
+    var name by xdStringProp()
+}
+
+class E1(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<E1>()
+
+    var e2 by xdLink0_1(E2, onTargetDelete = CLEAR)
+}
+
+class E2(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<E2>()
+}
+
+class F1(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<F1>()
+
+    val f2 by xdLink0_N(F2::f1, onTargetDelete = CASCADE, onDelete = CASCADE)
+}
+
+
+class F2(override val entity: Entity) : XdEntity() {
+    companion object : XdNaturalEntityType<F2>()
+
+    var f1: F1? by xdLink0_1(F1::f2, onDelete = CLEAR)
 }
--- dnq/src/test/kotlin/kotlinx.dnq/onTargetDelete/TargetDeleteTest.kt
+++ dnq/src/test/kotlin/kotlinx.dnq/onTargetDelete/TargetDeleteTest.kt
@@ -2,6 +2,7 @@ package kotlinx.dnq.onTargetDelete
 
 import kotlinx.dnq.DBTest
 import kotlinx.dnq.XdModel
+import kotlinx.dnq.query.first
 import kotlinx.dnq.query.isEmpty
 import kotlinx.dnq.query.size
 import org.junit.Assert.assertTrue
@@ -12,7 +13,7 @@ import kotlin.test.assertEquals
 class TargetDeleteTest : DBTest() {
 
     override fun registerEntityTypes() {
-        XdModel.registerNodes(A1, A2, A3)
+        XdModel.registerNodes(A1, A2, A3, B1, B2, B3, B4, C1, C2, D1, D2, E1, E2, F1, F2)
     }
 
     @Test
@@ -58,4 +59,80 @@ class TargetDeleteTest : DBTest() {
         }
     }
 
+    @Test
+    fun nestedCascade() {
+        val b4 = transactional {
+            B4.new { B1(B2(B3(this))) }
+        }
+        transactional { b4.delete() }
+        transactional {
+            assertEquals(0, B1.all().size())
+            assertEquals(0, B2.all().size())
+            assertEquals(0, B3.all().size())
+            assertEquals(0, B4.all().size())
+        }
+    }
+
+    @Test
+    fun simpleClear() {
+        val c1 = transactional {
+            C1.new { C2(this) }
+        }
+        transactional { c1.delete() }
+        transactional {
+            assertEquals(0, C1.all().size())
+            assertEquals(1, C2.all().size())
+            assertEquals(null, C2.all().first().c1)
+        }
+    }
+
+    @Test
+    fun clearAndCascade() {
+        val first = transactional {
+            val parent = D1.new()
+            D2("some")
+            D2("first").apply {
+                parent.d2.add(this)
+                parent.d2.add(D2("second"))
+            }
+        }
+        transactional { first.delete() }
+        transactional {
+            assertEquals(1, D1.all().size())
+            assertEquals(2, D2.all().size())
+        }
+    }
+
+    @Test
+    fun updateAndDelete() {
+        val (first, e2a, e2b) = transactional {
+            E1.new().let {
+                Triple(it, E2.new { it.e2 = this }, E2.new())
+            }
+        }
+        transactional {
+            first.e2 = e2b
+            e2a.delete()
+        }
+        transactional {
+            assertEquals(e2b, first.e2)
+        }
+    }
+
+    @Test
+    fun cascadeDelete() {
+        val leaf = transactional {
+            F1.new {
+                val leaf = F2.new()
+                f2.add(leaf)
+                f2.add(F2.new())
+                f2.add(F2.new())
+            }
+        }
+        transactional { leaf.delete() }
+        transactional {
+            assertEquals(0, F1.all().size())
+            assertEquals(0, F2.all().size())
+        }
+    }
 }
