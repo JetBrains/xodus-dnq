4979d521b8179f62ca9aab97d898058e04077c07
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-11-27 11:06:14 +0100

    Moving tests to Kotlin: DisposeCursorTest

--- dnq/src/test/kotlin/kotlinx.dnq/DisposeCursorTest.kt
+++ dnq/src/test/kotlin/kotlinx.dnq/DisposeCursorTest.kt
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.teamsys.dnq.database;
+package kotlinx.dnq;
 
 import com.jetbrains.teamsys.dnq.association.DirectedAssociationSemantics;
 import com.jetbrains.teamsys.dnq.association.PrimitiveAssociationSemantics;
+import com.jetbrains.teamsys.dnq.database.TransientStoreUtil;
 import com.jetbrains.teamsys.dnq.database.testing.TestBase;
 import jetbrains.exodus.database.TransientStoreSession;
 import jetbrains.exodus.dnq.util.TestUserService;
