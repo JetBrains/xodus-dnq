375e1c2c634e45762bc7e5484847ca93796f2e4f
Maxim Mazin <maxim.mazin@jetbrains.com>
2017-11-27 15:38:57 +0100

    Moving tests to Kotlin: TransientEntityLinksFromSetTest

--- dnq/src/test/kotlin/kotlinx.dnq/TransientEntityLinksFromSetTest.kt
+++ dnq/src/test/kotlin/kotlinx.dnq/TransientEntityLinksFromSetTest.kt
@@ -13,148 +13,100 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kotlinx.dnq;
+package kotlinx.dnq
 
-import com.jetbrains.teamsys.dnq.association.AssociationSemantics;
-import com.jetbrains.teamsys.dnq.association.DirectedAssociationSemantics;
-import com.jetbrains.teamsys.dnq.database.TransientStoreUtil;
-import com.jetbrains.teamsys.dnq.database.testing.TestBase;
-import jetbrains.exodus.core.dataStructures.hash.HashSet;
-import jetbrains.exodus.database.TransientEntity;
-import jetbrains.exodus.database.TransientStoreSession;
-import jetbrains.exodus.entitystore.Entity;
-import jetbrains.exodus.entitystore.iterate.EntityIteratorWithPropId;
-import org.junit.Test;
+import com.google.common.truth.Truth.assertThat
+import com.jetbrains.teamsys.dnq.association.AssociationSemantics
+import com.jetbrains.teamsys.dnq.association.DirectedAssociationSemantics
+import jetbrains.exodus.database.TransientEntity
+import jetbrains.exodus.database.TransientStoreSession
+import jetbrains.exodus.entitystore.Entity
+import jetbrains.exodus.entitystore.iterate.EntityIteratorWithPropId
+import org.junit.Test
+import kotlin.coroutines.experimental.buildSequence
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+class TransientEntityLinksFromSetTest : DBTest() {
 
-public class TransientEntityLinksFromSetTest extends TestBase {
+    class Issue(entity: Entity) : XdEntity(entity) {
+        companion object : XdNaturalEntityType<Issue>()
+    }
+
+    override fun registerEntityTypes() {
+        XdModel.registerNodes(Issue)
+    }
 
     @Test
-    public void testAll() {
-        TransientEntity i1, i2, i3, i4;
-        TransientStoreSession session = store.beginSession();
-        try {
-            i1 = createIssue(session);
-            i2 = createIssue(session);
-            i3 = createIssue(session);
-            i4 = createIssue(session);
-        } catch (Throwable e) {
-            TransientStoreUtil.abort(e, session);
-            throw new RuntimeException("Should never be thrown.");
-        } finally {
-            TransientStoreUtil.commit(session);
+    fun testAll() {
+        val (i1, i2, i3, i4) = transactional { txn ->
+            (1..4).map { txn.newEntity("Issue") }
         }
 
-        final HashSet<String> names = new HashSet<>();
-        names.add("dup");
-        names.add("hup");
+        val names = setOf("dup", "hup")
 
-        session = store.beginSession();
-        try {
-            DirectedAssociationSemantics.createToMany(i1, "dup", i2);
-            DirectedAssociationSemantics.createToMany(i1, "hup", i3);
-            DirectedAssociationSemantics.createToMany(i1, "hup", i4);
-            DirectedAssociationSemantics.createToMany(i2, "dup", i3);
+        transactional { txn ->
+            DirectedAssociationSemantics.createToMany(i2, "dup", i3)
+            DirectedAssociationSemantics.createToMany(i1, "hup", i4)
+            DirectedAssociationSemantics.createToMany(i1, "hup", i3)
+            DirectedAssociationSemantics.createToMany(i1, "dup", i2)
 
-            check_i1(AssociationSemantics.getAddedLinks(i1, names), i2, i3, i4);
-            check_i2(AssociationSemantics.getAddedLinks(i2, names), i3);
+            assertThat(i1.getAddedLinks(names).toNamesAndEntities())
+                    .containsExactly("dup" to i2, "hup" to i3, "hup" to i4, "hup" to null)
+            assertThat(i2.getAddedLinks(names).toNamesAndEntities())
+                    .containsExactly("dup" to i3, "dup" to null)
 
-            check_i1(AssociationSemantics.getAddedLinks(ro(i1, session), names), i2, i3, i4);
-            check_i2(AssociationSemantics.getAddedLinks(ro(i2, session), names), i3);
+            assertThat(i1.readonlyCopy(txn).getAddedLinks(names).toNamesAndEntities())
+                    .containsExactly("dup" to i2, "hup" to i3, "hup" to i4, "hup" to null)
+            assertThat(i2.readonlyCopy(txn).getAddedLinks(names).toNamesAndEntities())
+                    .containsExactly("dup" to i3, "dup" to null)
 
-            assertFalse(AssociationSemantics.getRemovedLinks(i1, names).iterator().hasNext());
-            assertFalse(AssociationSemantics.getRemovedLinks(i2, names).iterator().hasNext());
-        } catch (Throwable e) {
-            TransientStoreUtil.abort(e, session);
-            throw new RuntimeException("Should never be thrown.");
-        } finally {
-            TransientStoreUtil.commit(session);
+            assertThat(AssociationSemantics.getRemovedLinks(i1, names)).isEmpty()
+            assertThat(AssociationSemantics.getRemovedLinks(i2, names)).isEmpty()
         }
 
-        session = store.beginSession();
-        try {
-            check_i1(AssociationSemantics.getToMany(i1, names), i2, i3, i4);
-            check_i2(AssociationSemantics.getToMany(i2, names), i3);
-        } catch (Throwable e) {
-            TransientStoreUtil.abort(e, session);
-            throw new RuntimeException("Should never be thrown.");
-        } finally {
-            TransientStoreUtil.commit(session);
+        transactional {
+            assertThat(AssociationSemantics.getToMany(i1, names).toNamesAndEntities())
+                    .containsExactly("dup" to i2, "hup" to i3, "hup" to i4, "hup" to null)
+            assertThat(AssociationSemantics.getToMany(i2, names).toNamesAndEntities())
+                    .containsExactly("dup" to i3, "dup" to null)
         }
 
-        session = store.beginSession();
-        try {
-            DirectedAssociationSemantics.removeToMany(i1, "dup", i2);
-            DirectedAssociationSemantics.removeToMany(i1, "hup", i3);
-            DirectedAssociationSemantics.removeToMany(i1, "hup", i4);
-            DirectedAssociationSemantics.removeToMany(i2, "dup", i3);
+        transactional { txn ->
+            DirectedAssociationSemantics.removeToMany(i1, "dup", i2)
+            DirectedAssociationSemantics.removeToMany(i1, "hup", i3)
+            DirectedAssociationSemantics.removeToMany(i1, "hup", i4)
+            DirectedAssociationSemantics.removeToMany(i2, "dup", i3)
 
-            check_i1(AssociationSemantics.getRemovedLinks(i1, names), i2, i3, i4);
-            check_i2(AssociationSemantics.getRemovedLinks(i2, names), i3);
+            assertThat(AssociationSemantics.getRemovedLinks(i1, names).toNamesAndEntities())
+                    .containsExactly("dup" to i2, "hup" to i3, "hup" to i4, "hup" to null)
+            assertThat(AssociationSemantics.getRemovedLinks(i2, names).toNamesAndEntities())
+                    .containsExactly("dup" to i3, "dup" to null)
 
-            check_i1(AssociationSemantics.getRemovedLinks(ro(i1, session), names), i2, i3, i4);
-            check_i2(AssociationSemantics.getRemovedLinks(ro(i2, session), names), i3);
+            assertThat(AssociationSemantics.getRemovedLinks(i1.readonlyCopy(txn), names).toNamesAndEntities())
+                    .containsExactly("dup" to i2, "hup" to i3, "hup" to i4, "hup" to null)
+            assertThat(AssociationSemantics.getRemovedLinks(i2.readonlyCopy(txn), names).toNamesAndEntities())
+                    .containsExactly("dup" to i3, "dup" to null)
 
-            assertFalse(AssociationSemantics.getAddedLinks(i1, names).iterator().hasNext());
-            assertFalse(AssociationSemantics.getAddedLinks(i2, names).iterator().hasNext());
-        } catch (Throwable e) {
-            TransientStoreUtil.abort(e, session);
-            throw new RuntimeException("Should never be thrown.");
-        } finally {
-            TransientStoreUtil.commit(session);
+            assertThat(i1.getAddedLinks(names)).isEmpty()
+            assertThat(i2.getAddedLinks(names)).isEmpty()
         }
     }
 
     @Test
-    public void testWD_2060() {
-        TransientEntity i;
-        TransientStoreSession session = store.beginSession();
-        try {
-            i = ro(createIssue(session), session);
-            AssociationSemantics.getToManyList(i, "nonExistentLinkName");
-        } catch (Throwable e) {
-            TransientStoreUtil.abort(e, session);
-            throw new RuntimeException("Should never be thrown.");
-        } finally {
-            TransientStoreUtil.commit(session);
+    fun testWD_2060() {
+        transactional { txn ->
+            val i = txn.newEntity("Issue").readonlyCopy(txn)
+            assertThat(AssociationSemantics.getToManyList(i, "nonExistentLinkName")).isEmpty()
         }
     }
 
-    private static TransientEntity createIssue(TransientStoreSession session) {
-        return session.newEntity("Issue");
-    }
+    private fun TransientEntity.readonlyCopy(txn: TransientStoreSession) =
+            txn.transientChangesTracker.getSnapshotEntity(this)
 
-    private static TransientEntity ro(TransientEntity i1, TransientStoreSession session) {
-        return session.getTransientChangesTracker().getSnapshotEntity(i1);
-    }
-
-    private void check_i2(Iterable<Entity> iterable, TransientEntity i3) {
-        final EntityIteratorWithPropId it = (EntityIteratorWithPropId) iterable.iterator();
-        assertTrue(it.hasNext());
-        assertEquals(i3, it.next());
-        assertEquals("dup", it.currentLinkName());
-        assertFalse(it.hasNext());
-    }
-
-    private void check_i1(Iterable<Entity> iterable, TransientEntity i2, TransientEntity i3, TransientEntity i4) {
-        final EntityIteratorWithPropId it = (EntityIteratorWithPropId) iterable.iterator();
-        assertTrue(it.hasNext());
-        assertEquals(i2, it.next());
-        assertEquals("dup", it.currentLinkName());
-        assertTrue(it.hasNext());
-        final Entity candidate = it.next();
-        assertEquals("hup", it.currentLinkName());
-        assertTrue(it.hasNext());
-        if (i3.equals(candidate)) { // handle set nondeterminism
-            assertEquals(i4, it.next());
-        } else {
-            assertEquals(i4, candidate);
-            assertEquals(i3, it.next());
-        }
-        assertEquals("hup", it.currentLinkName());
-        assertFalse(it.hasNext());
+    private fun Iterable<Entity>.toNamesAndEntities(): List<Pair<String, Entity?>> {
+        return buildSequence {
+            val iterator = iterator() as EntityIteratorWithPropId
+            yieldAll(iterator.asSequence().map { iterator.currentLinkName() to it })
+            yield(iterator.currentLinkName() to null)
+        }.toList()
     }
 }
