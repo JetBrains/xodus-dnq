d57a868bd2b73d37b996a61ee5e296a971dcd516
lehvolk <alexey.volkov@jetbrains.com>
2017-12-27 13:30:00 +0300

    #38 cleanup

--- dnq/src/main/kotlin/kotlinx/dnq/util/Ids.kt
+++ dnq/src/main/kotlin/kotlinx/dnq/util/Ids.kt
@@ -3,11 +3,12 @@ package kotlinx.dnq.util
 import jetbrains.exodus.entitystore.EntityRemovedInDatabaseException
 import kotlinx.dnq.XdEntity
 import kotlinx.dnq.XdEntityType
+import kotlinx.dnq.session
 import kotlinx.dnq.toXd
 
 @Throws(EntityRemovedInDatabaseException::class)
 fun <T : XdEntity> XdEntityType<T>.findById(xdId: String): T {
-    val tx = (entityStore.threadSession
-            ?: throw IllegalStateException("finding entities can be called only in transactional block"))
-    return tx.getEntity(tx.toEntityId(xdId)).toXd()
+    return entityStore.session.let {
+        it.getEntity(it.toEntityId(xdId)).toXd()
+    }
 }
