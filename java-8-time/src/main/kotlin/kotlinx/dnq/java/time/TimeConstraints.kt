9c3492aceb96dc9000f7a9416f1fc00c0b60f4a9
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-01-02 18:19:35 +0300

    Added KDoc java-8-time property constraints.

--- java-8-time/src/main/kotlin/kotlinx/dnq/java/time/TimeConstraints.kt
+++ java-8-time/src/main/kotlin/kotlinx/dnq/java/time/TimeConstraints.kt
@@ -19,33 +19,81 @@ import com.jetbrains.teamsys.dnq.database.PropertyConstraint
 import kotlinx.dnq.simple.PropertyConstraintBuilder
 import java.time.temporal.Temporal
 
-fun <V> PropertyConstraintBuilder<*, V?>.isAfter(dateTime: () -> V, message: String = "is not after ${dateTime()}")
+/**
+ * Adds constraint for [Temporal] primitive persistent property.
+ * The property value should be more than the value returned by the given closure.
+ *
+ * Constrains are checked on transaction flush. Xodus-DNQ throws `ConstraintsValidationException` if constraint check
+ * fails. Method `getCauses()` of `ConstraintsValidationException` returns all actual
+ * `DataIntegrityViolationException`s corresponding to data validation errors that happen during the transaction flush.
+ * ```
+ * try {
+ *     store.transactional { /* Do some database update */ }
+ * } catch(e: ConstraintsValidationException) {
+ *     e.causes.forEach { e.printStackTrace() }
+ * }
+ * ```
+ *
+ * **Sample**
+ * ```
+ * var afterDomini by xdInstantProp { isAfter({ domini }) }
+ * ```
+ *
+ * @param time closure that returns date-time that should be less than the value of the property
+ * @param message optional error message that will be returned from
+ *        [jetbrains.exodus.database.exceptions.DataIntegrityViolationException#getDisplayMessage]
+ */
+fun <V> PropertyConstraintBuilder<*, V?>.isAfter(time: () -> V, message: String = "is not after ${time()}")
         where V : Comparable<V>,
               V : Temporal {
 
     constraints.add(object : PropertyConstraint<V?>() {
         override fun isValid(propertyValue: V?): Boolean {
-            return propertyValue == null || propertyValue > dateTime()
+            return propertyValue == null || propertyValue > time()
         }
 
         override fun getExceptionMessage(propertyName: String, propertyValue: V?) =
-                "$propertyName should be after ${dateTime()} but was $propertyValue"
+                "$propertyName should be after ${time()} but was $propertyValue"
 
         override fun getDisplayMessage(propertyName: String, propertyValue: V?) = message
     })
 }
 
-fun <V> PropertyConstraintBuilder<*, V?>.isBefore(dateTime: () -> V, message: String = "is not before ${dateTime()}")
+/**
+ * Adds constraint for [Temporal] primitive persistent property.
+ * The property value should be less than the value returned by the given closure.
+ *
+ * Constrains are checked on transaction flush. Xodus-DNQ throws `ConstraintsValidationException` if constraint check
+ * fails. Method `getCauses()` of `ConstraintsValidationException` returns all actual
+ * `DataIntegrityViolationException`s corresponding to data validation errors that happen during the transaction flush.
+ * ```
+ * try {
+ *     store.transactional { /* Do some database update */ }
+ * } catch(e: ConstraintsValidationException) {
+ *     e.causes.forEach { e.printStackTrace() }
+ * }
+ * ```
+ *
+ * **Sample**
+ * ```
+ * var beforeChrist by xdInstantProp { isBefore({ domini }) }
+ * ```
+ *
+ * @param time closure that returns date-time that should be greater than the value of the property
+ * @param message optional error message that will be returned from
+ *        [jetbrains.exodus.database.exceptions.DataIntegrityViolationException#getDisplayMessage]
+ */
+fun <V> PropertyConstraintBuilder<*, V?>.isBefore(time: () -> V, message: String = "is not before ${time()}")
         where V : Comparable<V>,
               V : Temporal {
 
     constraints.add(object : PropertyConstraint<V?>() {
         override fun isValid(propertyValue: V?): Boolean {
-            return propertyValue == null || propertyValue < dateTime()
+            return propertyValue == null || propertyValue < time()
         }
 
         override fun getExceptionMessage(propertyName: String, propertyValue: V?) =
-                "$propertyName should be before ${dateTime()} but was $propertyValue"
+                "$propertyName should be before ${time()} but was $propertyValue"
 
         override fun getDisplayMessage(propertyName: String, propertyValue: V?) = message
     })
