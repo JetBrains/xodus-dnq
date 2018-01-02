76bcc92ff35964b0075c056522372dd7d829aee9
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-01-02 23:01:00 +0300

    Added KDoc for NodeBase builder operations

--- dnq/src/main/kotlin/kotlinx/dnq/query/NodeBaseOperations.kt
+++ dnq/src/main/kotlin/kotlinx/dnq/query/NodeBaseOperations.kt
@@ -31,135 +31,277 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.jvm.javaType
 
+/**
+ * Returns new [NodeBase] representing negation of the given [node].
+ */
 fun not(node: NodeBase): NodeBase = UnaryNot(node)
 
+/**
+ * Returns new [NodeBase] representing conjunction of `this` and [that] nodes.
+ */
 infix fun NodeBase.and(that: NodeBase): NodeBase = And(this, that)
 
+/**
+ * Returns new [NodeBase] representing disjunction of `this` and [that] nodes.
+ */
 infix fun NodeBase.or(that: NodeBase): NodeBase = Or(this, that)
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] equal to the given [value].
+ */
 fun eq(dbPropertyName: String, value: Comparable<*>?): NodeBase {
     return PropertyEqual(dbPropertyName, value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property equal to the given [value].
+ */
 infix inline fun <reified R : XdEntity, T : Comparable<*>> KProperty1<R, T?>.eq(value: T?): NodeBase {
     return eq(this.getDBName(R::class), value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property equal to the given [value].
+ */
 infix inline fun <reified R : XdEntity> KProperty1<R, DateTime?>.eq(value: DateTime?): NodeBase {
     return eq(this.getDBName(R::class), value?.millis)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] is not equal to the given [value].
+ */
 fun ne(dbPropertyName: String, value: Comparable<*>?): NodeBase {
     return not(PropertyEqual(dbPropertyName, value))
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is not equal to the given [value].
+ */
 infix inline fun <reified R : XdEntity, T : Comparable<*>> KProperty1<R, T?>.ne(value: T?): NodeBase {
     return ne(this.getDBName(R::class), value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is not equal to the given [value].
+ */
 infix inline fun <reified R : XdEntity> KProperty1<R, DateTime?>.ne(value: DateTime?): NodeBase {
     return ne(this.getDBName(R::class), value?.millis)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] falls into `this` range.
+ */
 fun <T : Comparable<T>> ClosedRange<T>.contains(dbPropertyName: String): NodeBase {
     return PropertyRange(dbPropertyName, start, endInclusive)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the given [property] falls into `this` range.
+ */
 infix inline fun <reified R : XdEntity, T : Comparable<T>> ClosedRange<T>.contains(property: KProperty1<R, T?>): NodeBase {
     return contains(property.getDBName(R::class))
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the given [property] falls into `this` range.
+ */
 @JvmName("containsDate")
 infix inline fun <reified R : XdEntity> ClosedRange<DateTime>.contains(property: KProperty1<R, DateTime?>): NodeBase {
     return contains(property.getDBName(R::class))
 }
 
 /**
- * Filter elements with a value of the field greater than given `value`
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] is greater than the given [value].
  */
 fun <V : Comparable<V>> gt(dbPropertyName: String, value: V, valueKClass: KClass<V>): NodeBase {
     return PropertyRange(dbPropertyName, valueKClass.next(value), valueKClass.maxValue())
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is greater than the given [value].
+ */
 inline infix fun <reified R : XdEntity, reified V : Comparable<V>> KProperty1<R, V?>.gt(value: V): NodeBase {
     return gt(getDBName(R::class), value, V::class)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is greater than the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, DateTime?>.gt(value: DateTime): NodeBase {
     return gt(getDBName(R::class), value.millis, Long::class)
 }
 
 /**
- * Filter elements with a value of the field less than given `value`
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] is less than the given [value].
  */
 fun <V : Comparable<V>> lt(dbPropertyName: String, value: V, valueKClass: KClass<V>): NodeBase {
     return PropertyRange(dbPropertyName, valueKClass.minValue(), valueKClass.prev(value))
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is less than the given [value].
+ */
 inline infix fun <reified R : XdEntity, reified V : Comparable<V>> KProperty1<R, V?>.lt(value: V): NodeBase {
     return lt(getDBName(R::class), value, V::class)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is less than the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, DateTime?>.lt(value: DateTime): NodeBase {
     return lt(getDBName(R::class), value.millis, Long::class)
 }
 
 /**
- * Filter elements with a value of the field greater or equal to given `value`
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] is greater or equal to the given [value].
  */
 fun <V : Comparable<V>> ge(dbPropertyName: String, value: V, valueKClass: KClass<V>): PropertyRange {
     return PropertyRange(dbPropertyName, value, valueKClass.maxValue())
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is greater or equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity, reified V : Comparable<V>> KProperty1<R, V?>.ge(value: V): PropertyRange {
     return ge(getDBName(R::class), value, V::class)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is greater or equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, DateTime?>.ge(value: DateTime): PropertyRange {
     return ge(getDBName(R::class), value.millis, Long::class)
 }
 
 /**
- * Filter elements with a value of the field less or equal to given `value`
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the property with name [dbPropertyName] is less or equal to the given [value].
  */
 fun <V : Comparable<V>> le(dbPropertyName: String, value: V, valueKClass: KClass<V>): PropertyRange {
     return PropertyRange(dbPropertyName, valueKClass.minValue(), value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is less or equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity, reified V : Comparable<V>> KProperty1<R, V?>.le(value: V): PropertyRange {
     return le(getDBName(R::class), value, V::class)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is less or equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, DateTime?>.le(value: DateTime): PropertyRange {
     return le(getDBName(R::class), value.millis, Long::class)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of the String property with name [dbPropertyName] starts with the given [value].
+ */
 fun startsWith(dbPropertyName: String, value: String?): NodeBase {
     return PropertyStartsWith(dbPropertyName, value ?: "")
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` String property starts with the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, String?>.startsWith(value: String?): NodeBase {
     return startsWith(getDBName(R::class), value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property equal to the given [value].
+ */
 fun <R : XdEntity, T : XdEntity> KProperty1<R, T?>.eq(entityKClass: KClass<R>, value: T?): NodeBase {
     return LinkEqual(getDBName(entityKClass), value?.entity)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity, T : XdEntity> KProperty1<R, T?>.eq(value: T?): NodeBase {
     return eq(R::class, value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property contains the given [value].
+ */
 fun <R : XdEntity, T : XdEntity> KProperty1<R, XdQuery<T>>.contains(entityKClass: KClass<R>, value: T): NodeBase {
     return LinkEqual(getDBName(entityKClass), value.entity)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property contains the given [value].
+ */
 inline infix fun <reified R : XdEntity, T : XdEntity> KProperty1<R, XdQuery<T>>.contains(value: T): NodeBase {
     return contains(R::class, value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is not equal to the given [value].
+ */
 fun <R : XdEntity, T : XdEntity> KProperty1<R, T?>.ne(entityKClass: KClass<R>, value: T?): NodeBase = not(this.eq(entityKClass, value))
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property is not equal to the given [value].
+ */
 inline infix fun <reified R : XdEntity, T : XdEntity> KProperty1<R, T?>.ne(value: T?): NodeBase = ne(R::class, value)
 
 @Deprecated("Use matches(entityKClass, node) instead", ReplaceWith("this.matches(entityKClass, node)", "kotlinx.dnq.query.NodeBaseOperationsKt.matches"))
@@ -172,11 +314,31 @@ inline fun <reified R : XdEntity, T : XdEntity> KProperty1<R, T?>.link(node: Nod
     return matches(node)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property matches the given [node] predicate.
+ *
+ * ### Sample
+ * ```
+ * XdUser.query(XdUser::contact.matches(XdContact::isVerified eq true))
+ * ```
+ */
 fun <R : XdEntity, T : XdEntity> KProperty1<R, T?>.matches(entityKClass: KClass<R>, node: NodeBase): NodeBase {
     @Suppress("UNCHECKED_CAST")
     return LinksEqualDecorator(getDBName(entityKClass), node, (this.returnType.javaType as Class<T>).entityType.entityType)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` property matches the given [node] predicate.
+ *
+ * ### Sample
+ * ```
+ * XdUser.query(XdUser::contact.matches(XdContact::isVerified eq true))
+ * ```
+ */
 inline fun <reified R : XdEntity, T : XdEntity> KProperty1<R, T?>.matches(node: NodeBase): NodeBase {
     return matches(R::class, node)
 }
@@ -221,10 +383,20 @@ inline infix fun <reified T : XdEntity, reified R : Comparable<*>> KProperty1<T,
     return this.containsIn(*values.toList().toTypedArray())
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` Set property contains the given [value].
+ */
 infix inline fun <reified R : XdEntity, T : Comparable<T>> KProperty1<R, Set<T>>.contains(value: T): NodeBase {
     return eq(this.getDBName(R::class), value)
 }
 
+/**
+ * Returns new [NodeBase] representing filter:
+ *
+ * value of `this` Set<String> property contains element that starts with the given [value].
+ */
 inline infix fun <reified R : XdEntity> KProperty1<R, Set<String>>.anyStartsWith(value: String?): NodeBase {
     return startsWith(getDBName(R::class), value)
 }
