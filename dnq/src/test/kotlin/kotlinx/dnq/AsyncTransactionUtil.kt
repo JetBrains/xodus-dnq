fc66bb9bee95c52829c8c12ebfd43bd19cccfd9b
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-03-09 18:24:11 +0100

    Moved AutoMergeTest from webr-dnq

---
+++ dnq/src/test/kotlin/kotlinx/dnq/AsyncTransactionUtil.kt
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2006 - 2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kotlinx.dnq
+
+import jetbrains.exodus.database.TransientEntityStore
+import jetbrains.exodus.database.TransientStoreSession
+import kotlin.concurrent.thread
+
+fun <T> TransientEntityStore.runTranAsyncAndJoin(body: (TransientStoreSession) -> T): T {
+    var result: T? = null
+    val thread = thread(start = true) {
+        result = transactional(block = body)
+    }
+    thread.join()
+    return result as T
+}
---
+++ dnq/src/test/kotlin/kotlinx/dnq/AutoMergeTest.kt
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2006 - 2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kotlinx.dnq
+
+import jetbrains.exodus.entitystore.Entity
+import org.junit.Test
+
+class AutoMergeTest : DBTest() {
+
+    class Project(entity: Entity) : XdEntity(entity) {
+        companion object : XdNaturalEntityType<Project>()
+
+        val issues by xdLink0_N(Issue::project)
+    }
+
+    class Issue(entity: Entity) : XdEntity(entity) {
+        companion object : XdNaturalEntityType<Issue>() {
+            fun new(name: String) = new {
+                this.name = name
+            }
+        }
+
+        var name by xdStringProp()
+        var project: Project by xdParent(Project::issues)
+    }
+
+    override fun registerEntityTypes() {
+        XdModel.registerNodes(Project, Issue)
+    }
+
+    @Test
+    fun testAddIssueToProject() {
+        val p = transactional { Project.new() }
+        val (i1, i2) = store.runTranAsyncAndJoin {
+            val i1 = Issue.new("issue1")
+            p.issues.add(i1)
+            val i2 = store.runTranAsyncAndJoin {
+                val i2 = Issue.new("issue2")
+                p.issues.add(i2)
+                i2
+            }
+            Pair(i1, i2)
+        }
+        transactional {
+            assertThat(p.issues).containsExactly(i1, i2)
+        }
+    }
+}
