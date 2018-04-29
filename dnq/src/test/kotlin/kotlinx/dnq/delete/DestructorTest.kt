de9373aa7b29ea3dd46c32fbe035659331f487f1
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-04-29 12:24:09 +0200

    Moved DestructorsTest from webr-dnq

--- dnq/src/test/kotlin/kotlinx/dnq/delete/DestructorTest.kt
+++ dnq/src/test/kotlin/kotlinx/dnq/delete/DestructorTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kotlinx.dnq.delete.destructor
+package kotlinx.dnq.delete
 
 import com.google.common.truth.Truth.assertThat
 import jetbrains.exodus.entitystore.Entity
