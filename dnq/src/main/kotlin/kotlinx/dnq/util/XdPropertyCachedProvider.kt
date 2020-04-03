72c961961066f02dc1a6774584ddb1d70c89bd38
Vyacheslav Lukianov <lvo@intellij.net>
2020-04-03 14:52:03 +0300

    #67 KPropertyCachedProvider uses ConcurrentIntObjectCache in order to reduce memory traffic in provideDelegate()

--- dnq/src/main/kotlin/kotlinx/dnq/util/XdPropertyCachedProvider.kt
+++ dnq/src/main/kotlin/kotlinx/dnq/util/XdPropertyCachedProvider.kt
@@ -15,7 +15,7 @@
  */
 package kotlinx.dnq.util
 
-import jetbrains.exodus.core.dataStructures.ConcurrentObjectCache
+import jetbrains.exodus.core.dataStructures.ConcurrentIntObjectCache
 import kotlinx.dnq.XdEntity
 import kotlin.reflect.KProperty
 
@@ -25,27 +25,21 @@ class XdPropertyCachedProvider<out D>(private val create: () -> D) {
 
         private val cacheSize = System.getProperty("kotlinx.dnq.delegateProvider.cacheSize", "2000").toInt()
 
-        internal val cache = ConcurrentObjectCache<KPropertyHolder, Any>(cacheSize, 2)
+        internal val cache = ConcurrentIntObjectCache<KPropertyHolder<*>>(cacheSize, 2)
 
-        internal class KPropertyHolder(val prop: KProperty<*>) {
-
-            override fun hashCode(): Int {
-                return System.identityHashCode(prop)
-            }
-
-            override fun equals(other: Any?): Boolean {
-                return other is KPropertyHolder && prop === other.prop
-            }
-        }
+        internal class KPropertyHolder<D>(val prop: KProperty<*>, val delegate: D)
     }
 
+    @Suppress("UNCHECKED_CAST")
     operator fun provideDelegate(thisRef: XdEntity?, prop: KProperty<*>): D {
-        val key = KPropertyHolder(prop)
-        val instance = cache.tryKey(key) ?: return (create()).also {
-            cache.cacheObject(key, it!!)
+        val key = System.identityHashCode(prop)
+        cache.tryKey(key)?.let { ph ->
+            if (ph.prop === prop) {
+                return ph.delegate as D
+            }
         }
-
-        @Suppress("UNCHECKED_CAST")
-        return instance as D
+        return create().also { delegate ->
+            cache.cacheObject(key, KPropertyHolder(prop, delegate))
+        }
     }
 }
