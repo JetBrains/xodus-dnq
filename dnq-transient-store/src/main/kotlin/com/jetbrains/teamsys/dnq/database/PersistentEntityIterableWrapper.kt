858b51f4dbad3b528d0bef25081df7089400a1ae
Leonid Stryuk <leo.stryuk@gmail.com>
2024-12-19 14:27:34 +0100

    Removed some unnecessary class casts to OEntityIterableBase

--- dnq-transient-store/src/main/kotlin/com/jetbrains/teamsys/dnq/database/PersistentEntityIterableWrapper.kt
+++ dnq-transient-store/src/main/kotlin/com/jetbrains/teamsys/dnq/database/PersistentEntityIterableWrapper.kt
@@ -20,7 +20,6 @@ import jetbrains.exodus.database.TransientEntityStore
 import jetbrains.exodus.entitystore.*
 import jetbrains.exodus.entitystore.orientdb.OEntityIterable
 import jetbrains.exodus.entitystore.orientdb.iterate.OEntityIterableBase
-import jetbrains.exodus.entitystore.orientdb.iterate.link.OVertexEntityIterable
 import jetbrains.exodus.entitystore.orientdb.query.OSelect
 
 
@@ -129,8 +128,8 @@ open class PersistentEntityIterableWrapper(
         return store.threadSessionOrThrow.newEntity(this)
     }
 
-    override fun unwrap(): OEntityIterableBase {
-        return (wrappedIterable as? OEntityIterableBase) ?: (wrappedIterable.unwrap() as OEntityIterableBase)
+    override fun unwrap(): OEntityIterable {
+        return (wrappedIterable as? OEntityIterable) ?: (wrappedIterable.unwrap() as OEntityIterable)
     }
 
     override fun query(): OSelect {
