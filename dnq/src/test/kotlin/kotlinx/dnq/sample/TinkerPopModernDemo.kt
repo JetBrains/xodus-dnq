3b1ad3aa2b0caf322a09f2e4350caf18308c73a1
Lev Sivashov <lev.sivashov@jetbrains.com>
2025-09-25 19:23:20 +0200

    adding licence

--- dnq/src/test/kotlin/kotlinx/dnq/sample/TinkerPopModernDemo.kt
+++ dnq/src/test/kotlin/kotlinx/dnq/sample/TinkerPopModernDemo.kt
@@ -1,13 +1,26 @@
+/**
+ * Copyright 2006 - 2025 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kotlinx.dnq.sample
 
-import com.jetbrains.youtrackdb.api.DatabaseType
 import jetbrains.exodus.database.TransientEntityStore
 import jetbrains.exodus.entitystore.Entity
 import kotlinx.dnq.*
 import kotlinx.dnq.query.*
 import kotlinx.dnq.store.container.StaticStoreContainer
 import kotlinx.dnq.util.initMetaData
-import org.bouncycastle.util.io.pem.PemObjectParser
 import java.io.File
 
 // Version of "TinkerPop" modern Graph, but without weights on edges
