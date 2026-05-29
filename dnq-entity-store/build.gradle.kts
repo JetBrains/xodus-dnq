a05296a8ea0b0f7807192837e511ca8334982db9
Lev Sivashov <lev.sivashov@jetbrains.com>
2026-05-29 15:25:32 +0000

    Upgrade YouTrackDB to 0.5.0-20260529.123218-a7babc7-dev-SNAPSHOT

--- dnq-entity-store/build.gradle.kts
+++ dnq-entity-store/build.gradle.kts
@@ -1,4 +1,4 @@
-val ytdbVersion = "0.5.0-20260520.182352-cefda90-dev-SNAPSHOT"
+val ytdbVersion = "0.5.0-20260529.123218-a7babc7-dev-SNAPSHOT"
 
 val ktorVersion = "3.1.3"
 val graalVmVersion = "22.0.0.2"
